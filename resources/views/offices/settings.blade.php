79b997ce6889cc1c3eac14a2b3967e2bce971817
ibrahim-good-abilities <ibrahim.eldesoki@goodabilities.com>
2019-10-27 15:26:41 +0200

    Transaltion

--- resources/views/offices/settings.blade.php
+++ resources/views/offices/settings.blade.php
@@ -5,7 +5,7 @@
 
 @section('settings')
 <div class="col s2 m6 l6 right-align">
-    <a class="btn mb-1 waves-effect waves-light" href="{{ route('all_offices') }}">{{__('Back') }}
+    <a class="btn mb-1 waves-effect waves-light" href="#">{{__('Back') }}
         <i class="material-icons right">keyboard_return</i>
     </a>
 </div>

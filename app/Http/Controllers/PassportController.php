f120dd81437113bcf2e3b37742a2ddcf64c4b0f1
ibrahim-good-abilities <ibrahim.eldesoki@goodabilities.com>
2019-10-28 14:07:52 +0200

    api

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -1,10 +1,15 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use App\User;
+use App\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Password;
+
 class PassportController extends Controller
 {
     /**
@@ -19,20 +24,45 @@ class PassportController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8'],
+            'userProfilePicture' =>['required','image'],
+            'userIdFile'=>['required','file'],
+            'cityId' => ['required'],
+            'userAddress' => ['required'],
+             'userIdNum' => ['required','max:14'],
+            'userJopTitle' => 'required',
         ]);
 
         $user = new User();
+        $file = $request->file('userIdFile');
+        $fileSaveAsName = time() . "usersFiles." .$file->getClientOriginalExtension();
+        $upload_path = 'usersFiles/Id';
+        $file_url = $upload_path . $fileSaveAsName;
+        $file->move($upload_path, $fileSaveAsName);
+        //profile picture upload
+        $file = $request->file('userProfilePicture');
+        $fileSaveName = time() . "usersFiles." .$file->getClientOriginalExtension();
+        $uploadPath = 'usersFiles/profilePicture';
+        $profilePicture = $uploadPath . $fileSaveName;
+        $file->move($uploadPath, $fileSaveName);
+
         $user->name = $request->name;
         $user->email =  $request->email;
         $user->password = Hash::make( $request->password);
         $user->roleId = 4;
+        $user->userIdFile = $file_url;
+        $user->userProfilePicture=$profilePicture;
+        $user->cityId = $request->cityId;
+        $user->userAddress = $request->userAddress;
+        $user->userIdNum = $request->userIdNum;
+        $user->userJopTitle = $request->userJopTitle;
         $user->save();
+
         $token = $user->createToken('TutsForWeb')->accessToken;
         $user->api_token = $token;
         $user->save();
         return response()->json(['user' => $user], 200);
     }
- 
+
     /**
      * Handles Login Request
      *
@@ -45,7 +75,7 @@ class PassportController extends Controller
             'email' => $request->email,
             'password' => $request->password
         ];
- 
+
         if (auth()->attempt($credentials)) {
             $token = auth()->user()->createToken('TutsForWeb')->accessToken;
             $user = auth()->user();
@@ -56,7 +86,7 @@ class PassportController extends Controller
             return response()->json(['error' => 'UnAuthorised'], 401);
         }
     }
- 
+
     /**
      * Returns Authenticated User Details
      *
@@ -66,4 +96,30 @@ class PassportController extends Controller
     {
         return response()->json(['name' => 'hussam'], 200);
     }
+    public function cities()
+    {
+        $cities = City::all();
+        $cities_list=[];
+        foreach($cities as $city)
+        {
+            $offices =DB::table('offices')->where('cityId','=',$city->id)->get();
+            $cities_list[] = ['city_id'=>$city->id,'city_name'=>$city->cityName,'offices'=>$offices];
+        }
+
+        return response()->json(['cities'=>$cities_list]);
+    }
+    public function resetpassword(Request $request){
+       // $this->broker()->sendResetLink();
+        $response = Password::sendResetLink(['email' => $request->email], function (Message $message) {
+        $message->subject($this->getEmailSubject());
+    });
+
+    //return response()->json(['status'=>'success']);
+    switch ($response) {
+        case Password::RESET_LINK_SENT:
+            return response()->json(['status','success ']);
+        case Password::INVALID_USER:
+            return  response()->json(['status error' => 'invaild email']);
+    }
+}
 }

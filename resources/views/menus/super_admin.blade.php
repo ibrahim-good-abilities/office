3445f924663b8d0c73815dda720a70e73bba3a7e
Hussam Hussien <7@>
2019-10-29 14:26:51 +0200

    fix small issues

--- resources/views/menus/super_admin.blade.php
+++ resources/views/menus/super_admin.blade.php
@@ -1,69 +1,69 @@
 <li class="bold">
 
-        <a class="waves-effect waves-cyan " href="{{route('dashboard')}}"><i class="material-icons">settings_input_svideo</i><span class="menu-title" data-i18n="">{{ __('Welcome') }}</span></a>
+        <a class="waves-effect waves-cyan " href="{{route('dashboard')}}"><i class="material-icons">dashboard</i><span class="menu-title" data-i18n="">{{ __('Welcome') }}</span></a>
     </li>
-    <li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">playlist_add_check</i>
+    <li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">location_on</i>
         <span class="menu-title" data-i18n="">{{ __('Cities') }}</span></a>
         <div class="collapsible-body">
             <ul class="collapsible collapsible-sub" data-collapsible="accordion">
                     <li>
-                        <a class="collapsible-body" href="{{route('all_cities')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('all_cities')}}" data-i18n=""><i class="material-icons">list</i>
                                 <span> {{ __('Cities') }}</span>
                         </a>
                     </li>
                     <li>
-                        <a class="collapsible-body" href="{{route('create_city')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('create_city')}}" data-i18n=""><i class="material-icons">add_circle</i>
                                 <span> {{ __('Add New City') }}</span>
                         </a>
                     </li>
             </ul>
         </div>
 </li>
-<li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">playlist_add_check</i>
+<li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">home</i>
         <span class="menu-title" data-i18n="">{{ __('Offices') }}</span></a>
         <div class="collapsible-body">
             <ul class="collapsible collapsible-sub" data-collapsible="accordion">
                     <li>
-                        <a class="collapsible-body" href="{{route('all_offices')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('all_offices')}}" data-i18n=""><i class="material-icons">list</i>
                                 <span> {{ __('Offices') }}</span>
                         </a>
                     </li>
                     <li>
-                        <a class="collapsible-body" href="{{route('create_office')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('create_office')}}" data-i18n=""><i class="material-icons">add_circle</i>
                                 <span> {{ __('Add New Office') }}</span>
                         </a>
                     </li>
             </ul>
         </div>
 </li>
-<li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">playlist_add_check</i>
+<li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">lightbulb_outline</i>
         <span class="menu-title" data-i18n="">{{ __('Services') }}</span></a>
         <div class="collapsible-body">
             <ul class="collapsible collapsible-sub" data-collapsible="accordion">
                     <li>
-                        <a class="collapsible-body" href="{{route('all_services')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('all_services')}}" data-i18n=""><i class="material-icons">list</i>
                                 <span> {{ __('Services') }}</span>
                         </a>
                     </li>
                     <li>
-                        <a class="collapsible-body" href="{{route('create_service')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('create_service')}}" data-i18n=""><i class="material-icons">add_circle</i>
                                 <span> {{ __('Add New Service') }}</span>
                         </a>
                     </li>
             </ul>
         </div>
 </li>
-<li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">playlist_add_check</i>
+<li class="bold"><a class="collapsible-header waves-effect waves-cyan" href="javascript:void(0)"><i class="material-icons">security</i>
         <span class="menu-title" data-i18n="">{{ __('Roles') }}</span></a>
         <div class="collapsible-body">
             <ul class="collapsible collapsible-sub" data-collapsible="accordion">
                     <li>
-                        <a class="collapsible-body" href="{{route('all_roles')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('all_roles')}}" data-i18n=""><i class="material-icons">list</i>
                                 <span> {{ __('Roles') }}</span>
                         </a>
                     </li>
                     <li>
-                        <a class="collapsible-body" href="{{route('add_role')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('add_role')}}" data-i18n=""><i class="material-icons">add_circle</i>
                                 <span> {{ __('Add New Role') }}</span>
                         </a>
                     </li>
@@ -76,12 +76,12 @@
         <div class="collapsible-body">
             <ul class="collapsible collapsible-sub" data-collapsible="accordion">
                     <li>
-                        <a class="collapsible-body" href="{{route('all_users')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i>
+                        <a class="collapsible-body" href="{{route('all_users')}}" data-i18n=""><i class="material-icons">list</i>
                         <span>{{ __('Users') }}</span>
                     </a>
                     </li>
                     <li>
-                        <a class="collapsible-body" href="{{route('add_user')}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{ __('Add New User') }}</span></a>
+                        <a class="collapsible-body" href="{{route('add_user')}}" data-i18n=""><i class="material-icons">add_circle</i><span>{{ __('Add New User') }}</span></a>
                     </li>
             </ul>
         </div>

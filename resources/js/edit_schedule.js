04f56a03bc85b1dbba14fa34faa9ac6dc3d2416f
Hussam Hussien <7@>
2019-10-30 21:07:20 +0200

    fix official time

--- resources/js/edit_schedule.js
+++ resources/js/edit_schedule.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    if ($("[name='officialTime']").is(':checked')) {
+        $('.official').attr('disabled', 'disabled');
+    }
+
+    $("[name='officialTime']").on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.official').attr('disabled', 'disabled');
+        } else {
+            $('.official').removeAttr('disabled');
+        }
+    });
+
     $('.timepicker').timepicker();
-  });
+});
